21cb3deccbf8598427814ca724127bd7e44cf37b
kshyun1223 <kshyun.dev@gmail.com>
2023-03-09 21:30:38 +0900

    recoilStore.tsx

--- src/recoil/recoilStore.tsx
+++ src/recoil/recoilStore.tsx
@@ -1,15 +1,21 @@
+/* libs */
 import { atom, selector } from 'recoil'
 
+
+/* 할 일 목록을 담는 저장소 */
 export const todoListState = atom({
   key: 'todoListState',
   default: [],
 })
 
+/* 필터 항목을 담는 저장소 */
 export const todoListFilterState = atom({
   key: 'todoListFilterState',
   default: 'Show All',
-});
+})
 
+
+/* 필터링 된 목록을 만들어서 반환하는 함수 */
 export const filteredTodoListState = selector({
   key: 'filteredTodoListState',
   get: ({get}) => {
@@ -27,20 +33,25 @@ export const filteredTodoListState = selector({
   },
 });
 
+/* 저장소 내용에 대한 통계를 내서 반환하는 함수 */
 export const todoListStatsState = selector({
   key: 'todoListStatsState',
   get: ({get}) => {
-    const todoList = get(todoListState);
-    const totalNum = todoList.length;
-    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
-    const totalUncompletedNum = totalNum - totalCompletedNum;
-    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
+    /* 모든 할 일 */
+    const todoList = get(todoListState)
+    const totalNum = todoList.length
+    /* 완료된 할 일 */
+    const totalCompletedNum = todoList.filter((item) => item.isComplete).length
+    /* 아직 하지 않은 일 */
+    const totalUncompletedNum = totalNum - totalCompletedNum
+    /* 완료한 비율 */
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum
 
     return {
       totalNum,
       totalCompletedNum,
       totalUncompletedNum,
       percentCompleted,
-    };
+    }
   },
-});
+})
