f9df1a11fbe37b1d9d19efaa8b262f5edffb875a
kshyun1223 <kshyun.dev@gmail.com>
2023-03-09 21:31:56 +0900

    TodoListFilters.tsx

--- src/components/TodoList/TodoListFilters.tsx
+++ src/components/TodoList/TodoListFilters.tsx
@@ -1,23 +1,28 @@
-import React from "react";
-import { useRecoilState } from "recoil";
-import { todoListFilterState } from "../../recoil/recoilStore";
+/* libs */
+import React from "react"
+import { useRecoilState } from "recoil"
+
+/* recoilStore */
+import { todoListFilterState } from "../../recoil/recoilStore"
 
 
-export function TodoListFilters() {
-  const [filter, setFilter] = useRecoilState(todoListFilterState);
+export default function TodoListFilters() {
+  const [filter, setFilter] = useRecoilState(todoListFilterState) // 리코일 setter 호출
 
+  /* 드롭다운 버튼 */
   const updateFilter = ({target: {value}}) => {
-    setFilter(value);
-  };
+    setFilter(value) // 리코일 setter 실행
+  }
 
   return (
     <>
-      Filter:
+      <div>TodoListFilters</div>
+      <>필터: </>
       <select value={filter} onChange={updateFilter}>
-        <option value="Show All">All</option>
-        <option value="Show Completed">Completed</option>
-        <option value="Show Uncompleted">Uncompleted</option>
+        <option value="Show All">모든 할 일</option>
+        <option value="Show Completed">완료된 할 일</option>
+        <option value="Show Uncompleted">아직 하지 않은 일</option>
       </select>
     </>
-  );
+  )
 }
