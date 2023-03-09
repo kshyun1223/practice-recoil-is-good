a037929761ffa2bb359e31ed1fe16503c83176db
kshyun1223 <kshyun.dev@gmail.com>
2023-03-09 21:31:37 +0900

    TodoListStats.tsx

--- src/components/TodoList/TodoListStats.tsx
+++ src/components/TodoList/TodoListStats.tsx
@@ -1,23 +1,30 @@
-import React from "react";
-import { useRecoilValue } from "recoil";
-import { todoListStatsState } from "../../recoil/recoilStore";
+/* libs */
+import React from "react"
+import { useRecoilValue } from "recoil"
 
-export function TodoListStats() {
+/* recoilStore */
+import { todoListStatsState } from "../../recoil/recoilStore"
+
+/* 상단의 통계 */
+export default function TodoListStats() {
+  /* 리코일 저장소의 값을 구독 */
   const {
     totalNum,
     totalCompletedNum,
     totalUncompletedNum,
     percentCompleted,
-  } = useRecoilValue(todoListStatsState);
+  } = useRecoilValue(todoListStatsState)
 
-  const formattedPercentCompleted = Math.round(percentCompleted * 100);
+  const formattedPercentCompleted = Math.round(percentCompleted * 100)
 
   return (
-    <ul>
-      <li>Total items: {totalNum}</li>
-      <li>Items completed: {totalCompletedNum}</li>
-      <li>Items not completed: {totalUncompletedNum}</li>
-      <li>Percent completed: {formattedPercentCompleted}</li>
-    </ul>
-  );
+    <>
+      <ul> TodoListStats
+        <li>모든 할 일: {totalNum}</li>
+        <li>완료된 할 일: {totalCompletedNum}</li>
+        <li>아직 하지 않은 일: {totalUncompletedNum}</li>
+        <li>완료한 비율: {formattedPercentCompleted}</li>
+      </ul>
+    </>
+  )
 }
