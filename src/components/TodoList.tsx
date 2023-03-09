85307e00aa1ef17ea3fbce035727c879aee3ea22
kshyun1223 <kshyun.dev@gmail.com>
2023-03-09 21:31:06 +0900

    TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+/* libs */
+import React from "react"
 import { useRecoilValue } from "recoil"
-import { todoListState } from "../recoil/recoilStore";
-import TodoItem from "./TodoList/TodoItem"
-import TodoItemCreator from "./TodoList/TodoItemCreator";
-import { TodoListFilters } from "./TodoList/TodoListFilters";
-import { TodoListStats } from "./TodoList/TodoListStats";
 
+/* recoilStore */
+import { filteredTodoListState } from "../recoil/recoilStore"
+
+/* components */
+import TodoListStats from "./TodoList/TodoListStats"
+import TodoListFilters from "./TodoList/TodoListFilters"
+import TodoItemCreator from "./TodoList/TodoItemCreator"
+import TodoItem from "./TodoList/TodoItem"
+
+/* todo 기능 최상위 컨테이너 */
 export default function TodoList() {
-  const todoList = useRecoilValue(todoListState);
-  console.log(todoList)
+  /* 리코일 저장소의 값을 구독 */
+  const todoList = useRecoilValue(filteredTodoListState)
 
   return (
     <>
@@ -16,9 +22,10 @@ export default function TodoList() {
       <TodoListFilters />
       <TodoItemCreator />
 
+      <br /><div>TodoList</div>
       {todoList.map((todoItem) => (
         <TodoItem item={todoItem} key={todoItem.id} />
       ))}
     </>
-  );
+  )
 }
