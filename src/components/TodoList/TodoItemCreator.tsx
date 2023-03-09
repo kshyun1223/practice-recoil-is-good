498b785cd3c9f65ebf880c2531f79fc33afc67a3
kshyun1223 <kshyun.dev@gmail.com>
2023-03-09 21:32:08 +0900

    TodoItemCreator.tsx

--- src/components/TodoList/TodoItemCreator.tsx
+++ src/components/TodoList/TodoItemCreator.tsx
@@ -1,37 +1,47 @@
-import React, { useState }from "react";
+/* libs */
+import React, { useState }from "react"
 import { useSetRecoilState } from "recoil"
-import { todoListState } from "../../recoil/recoilStore";
 
+/* recoilStore */
+import { todoListState } from "../../recoil/recoilStore"
+
+/* 새 항목을 입력하는 input */
 export default function TodoItemCreator() {
-  const [inputValue, setInputValue] = useState('');
-  const setTodoList = useSetRecoilState(todoListState);
+  /* 임시 저장소 */
+  const [inputValue, setInputValue] = useState('')
+  
+  /* 리코일 setter 호출 */
+  const setTodoList = useSetRecoilState(todoListState)
 
+  /* 추가 버튼 */
   const addItem = () => {
-    setTodoList((oldTodoList) => [
+    setTodoList((oldTodoList) => [ // 리코일 setter 실행
       ...oldTodoList,
       {
         id: getId(),
         text: inputValue,
         isComplete: false,
       },
-    ]);
-    setInputValue('');
-  };
+    ])
+    setInputValue('') // 임시 저장소 초기화
+  }
 
+  /* 입력 내용을 실시간으로 임시 저장소에 반영 */
   const onChange = ({target: {value}}) => {
-    setInputValue(value);
-  };
+    setInputValue(value)
+  }
 
   return (
     <div>
+      <br /><div>TodoItemCreator</div>
       <input type="text" value={inputValue} onChange={onChange} />
-      <button onClick={addItem}>Add</button>
+      <button onClick={addItem}>추가</button>
     </div>
-  );
+  )
 }
 
-// 고유한 Id 생성을 위한 유틸리티
-let id = 0;
+/* 고유한 Id 생성을 위한 유틸리티 */
+let id = 0
 function getId() {
-  return id++;
+  return id++
 }
